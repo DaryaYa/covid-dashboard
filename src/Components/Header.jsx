687cec6ba17c453265fda08a41a8f08e76eb7709
Vladislav0101 <by.vlad01@mail.ru>
2020-12-16 19:37:40 +0300

    init: basic-layout

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,9 +2,11 @@ import React from "react";
 // import ReactDOM from "react-dom";
 
 export const Header = () => {
-  return (
-    <div className="header">
-      <h1>Hello from Header Component</h1>
-    </div>
-  );
+    return (
+        <header>
+            <h1 style={{ color: "#b9b9b9", textAlign: "center" }}>
+                COVID-2020
+            </h1>
+        </header>
+    );
 };
