d0a118b60296c44851ede3731284714782f55714
DaryaYa <lyalyaka@mail.ru>
2020-12-16 18:37:44 +0300

    feat: add styles

--- src/Components/ShowTotalCases.jsx
+++ src/Components/ShowTotalCases.jsx
@@ -1,12 +1,8 @@
-import React from "react";
-
 export const ShowTotalCases = (props) => {
-    return (
-        <div>
-            <div>
-                <p>Total Cases</p>
-                <p>{props.currentCountryTotalConfirmed}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="total-cases">
+      <p>Total Cases</p>
+      <p>{props.currentCountryTotalConfirmed}</p>
+    </div>
+  );
 };
