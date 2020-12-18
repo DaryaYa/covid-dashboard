8f273427da88a02eeb4c78001d7a31d3136aae1a
DaryaYa <lyalyaka@mail.ru>
2020-12-19 02:18:01 +0300

    refactor

--- src/Components/ListCountries.jsx
+++ src/Components/ListCountries.jsx
@@ -31,15 +31,18 @@ export const ListCountries = ({ countries, title, setTitle }) => {
    
     return (
       <div>
-        <button onClick={() => setTitle("deaths")}>deaths</button>
-        <button onClick={() => setTitle("recovered")}>recovered</button>
-        <button onClick={() => setTitle("cases")}>cases</button>
-        {/* <button onClick={this.onClickDeaths}>deaths</button>  */}
+        <div className={styles.buttons}>
+          <button onClick={() => setTitle("deaths")}>Deaths</button>
+        <button onClick={() => setTitle("recovered")}>Recovered</button>
+        <button onClick={() => setTitle("cases")}>Total cases</button>
+        </div>
+        
         <ul>
-          {sortByTotal(countries, title).map((country) => (
+          {sortByTotal(countries, title).map((country, index) => (
             <li
               className={styles.li}
-              key={country.countryInfo._id}
+              // key={country.countryInfo._id}
+              key={index}
               // onClick={() => this.setCountry(country)}
             >
               <p>
