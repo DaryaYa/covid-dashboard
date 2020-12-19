e01d55463cde95d02dcb06db9d29e96d3586cf24
Valentin Anikin <anival-94@mail.ru>
2020-12-19 17:24:46 +0300

    feat: include table into layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
-import { useState, useEffect, useCallback } from "react";
-import { Header } from "./Components/Header";
-import { ListCountries } from "./Components/ListCountries";
-import { MapBox } from "./Components/MapBox";
-import { ShowTotalCases } from "./Components/ShowTotalCases";
-import { ChartBox } from "./Components/ChartBox";
+/* eslint-disable react/react-in-jsx-scope */
+import { useState, useEffect, useCallback } from 'react';
+import { Header } from './Components/Header';
+import { ListCountries } from './Components/ListCountries';
+import { MapBox } from './Components/MapBox';
+import { ShowTotalCases } from './Components/ShowTotalCases';
+import { ChartBox } from './Components/ChartBox';
+import TableBox from './Components/tableBox/TableBox';
 
-import styles from "./app.module.scss";
+import styles from './app.module.scss';
 
 // const API = "https://api.covid19api.com/summary";
-const API = "https://corona.lmao.ninja/v2/countries";
+const API = 'https://corona.lmao.ninja/v2/countries';
 
 function App() {
+  const dataCategories = {
+    dataType: ['Cases', 'Deaths', 'Recovered'],
+    period: ['For all time', 'For the last day'],
+    territory: ['World', 'Country'],
+    population: ['Total', 'Per 100,000 population'],
+  };
+
+  const [currentPeriodIndex, setCurrentPeriodIndex] = useState(0);
+  const [currentTerritoryIndex, setCurrentTerritoryIndex] = useState(0);
+  const [currentPopulationIndex, setCurrentPopulationIndex] = useState(0);
+
+  function changeCategoryStatus(currentStatus) {
+    return currentStatus === 0 ? 1 : 0;
+  }
+
   const [data, setData] = useState({ countries: [] });
+
   useEffect(() => {
     const fetchData = async () => {
+      // eslint-disable-next-line no-undef
       const response = await fetch(API);
       const result = await response.json();
       console.log(result);
@@ -25,8 +44,8 @@ function App() {
 
   const [currentCountry, setCurrentCountry] = useState(null);
 
-  const setCountry = useCallback((currentCountry) => {
-    setCurrentCountry(currentCountry);
+  const setCountry = useCallback((country) => {
+    setCurrentCountry(country);
   }, []);
 
   return (
@@ -40,15 +59,28 @@ function App() {
             <ListCountries countries={data.countries} setCountry={setCountry} />
           </section>
         </div>
-        <MapBox></MapBox>
+        <MapBox />
         <div className={styles.showTotalCasesWrapper}>
-          {currentCountry && (
+          {/* {currentCountry && (
             <ShowTotalCases
               currentCountryTotalConfirmed={currentCountry.cases}
-            ></ShowTotalCases>
-          )}
+            />
+          )} */}
+          <TableBox
+            currentCountry={currentCountry}
+            dataCategories={dataCategories}
+            currentPopulationIndex={currentPopulationIndex}
+            setCurrentPopulationIndex={setCurrentPopulationIndex}
+            currentTerritoryIndex={currentTerritoryIndex}
+            setCurrentTerritoryIndex={setCurrentTerritoryIndex}
+            currentPeriodIndex={currentPeriodIndex}
+            setCurrentPeriodIndex={setCurrentPeriodIndex}
+            changeCategoryStatus={changeCategoryStatus}
+            countries={data.countries}
+            setCountry={setCountry}
+          />
         </div>
-        <ChartBox></ChartBox>
+        <ChartBox />
       </main>
     </div>
   );
