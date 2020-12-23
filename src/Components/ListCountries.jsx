718f50bd76340ee34d3c2e1f0314f17117e6ea0b
Vladislav0101 <by.vlad01@mail.ru>
2020-12-23 21:16:46 +0300

    map

--- src/Components/ListCountries.jsx
+++ src/Components/ListCountries.jsx
@@ -1,39 +1,40 @@
 import styles from "./ListCountries.module.css";
 
 export const ListCountries = ({ countries, title, setTitle, setCountry }) => {
-  function sortByTotal(arr, x) {
-    arr.sort((a, b) => b[x] - a[x]);
-    return arr;
-  }
+    function sortByTotal(arr, x) {
+        arr.sort((a, b) => b[x] - a[x]);
+        return arr;
+    }
 
-  return (
-    <div>
-      <div className={styles.buttons}>
-        <button onClick={() => setTitle("deaths")}>Deaths</button>
-        <button onClick={() => setTitle("recovered")}>Recovered</button>
-        <button onClick={() => setTitle("cases")}>Total cases</button>
-      </div>
+    return (
+        <div>
+            <div className={styles.buttons}>
+                <button onClick={() => setTitle("deaths")}>Deaths</button>
+                <button onClick={() => setTitle("recovered")}>Recovered</button>
+                <button onClick={() => setTitle("cases")}>Total cases</button>
+            </div>
 
-      <ul>
-        {sortByTotal(countries, title).map((country, index) => (
-          <li
-            className={styles.li}
-            // key={country.countryInfo._id}
-            key={index}
-            onClick={() => setCountry(country)}
-          >
-            <p>
-              <span className={styles.info}>{country[title]}</span>
-              <span>{country.country}</span>
-              <img
-                className={styles.img}
-                src={country.countryInfo.flag}
-                alt="flag"
-              />
-            </p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+            <ul>
+                {sortByTotal(countries, title).map((country, index) => (
+                    <li
+                        className={styles.li}
+                        key={index}
+                        onClick={() => setCountry(country)}
+                    >
+                        <p>
+                            <span className={styles.info}>
+                                {country[title]}
+                            </span>
+                            <span>{country.country}</span>
+                            <img
+                                className={styles.img}
+                                src={country.countryInfo.flag}
+                                alt="flag"
+                            />
+                        </p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
--- src/Components/MapBox.jsx
+++ src/Components/MapBox.jsx
@@ -1,18 +1,23 @@
 import { MapComponent } from "./MapComponent";
+import { MapSetValue } from "./MapSetValue";
+import styles from "./MapBox.module.css";
+import { useState } from "react";
+import { Legend } from "./Legend";
 
 export const MapBox = ({ countriesInfo }) => {
+    const [value, setValue] = useState("deaths");
     return (
-        <div
-            id="mapBox"
-            style={{
-                gridArea: "1/2/3/3",
-                overflow: "hidden",
-            }}
-        >
+        <div className={styles.mapBox}>
+            <MapSetValue
+                arrValues={["Cases", "Deaths", "Recovered"]}
+                setValue={setValue}
+            ></MapSetValue>
             <MapComponent
                 countriesInfo={countriesInfo}
-                styles={{ width: "50px" }}
+                className={styles.mapComponent}
+                value={value}
             ></MapComponent>
+            <Legend></Legend>
         </div>
     );
 };
